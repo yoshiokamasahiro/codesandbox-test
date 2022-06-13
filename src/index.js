2f1a04776bb2df018d6b5a9c39ab15bdb02957d1
yoshiokamasahiro <34766492+yoshiokamasahiro@users.noreply.github.com>
2022-06-13 16:33:10 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -45,10 +45,26 @@
 // console.log(val4);
 
 //constで定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-// val5[2] = "bird";
-val5.push("monky");
-val5.push("Lion");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// // val5[2] = "bird";
+// val5.push("monky");
+// val5.push("Lion");
+// console.log(val5);
 //【結論】オブジェクトや配列はconstで宣言していても中身を変更することができる
+
+/**
+ * テンプレート文字列
+ */
+const name = "ぺえ";
+const age = 37;
+//私はぺえです。37歳です。
+
+//従来の方法
+const message1 = "私は" + name + "です。" + age + "歳です。";
+console.log(message1);
+
+//テンプレート文字列を用いた方法
+//`(バッククォート)=>shift + @
+const message2 = `私の名前は${name}です。${age}歳です。`;
+console.log(message2);
